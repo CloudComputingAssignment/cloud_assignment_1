0f79014ea459fd9e4c7072c309054fadbe47e06e
Wasim Suleman <wasimsuleman.ws@gmail.com>
2024-03-17 15:57:06 +0500

    Database changes

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,12 +1,21 @@
 import mysql.connector
 
 config = {
-    'host':'cloud-assignment-1.database.windows.net',
-    'port':'1433',
-    'user':'ommair-01',
-    'password':'Omm@ir510219900',
-    'database':'user_database'
+    'host':'srv1100.hstgr.io',
+    'port':'3306',
+    'user':'u317157586_cc_asg',
+    'password':'$:XPL7VrH6',
+    'database':'u317157586_cc_asg'
 }
+
+# config = {
+#     'host':'cloud-assignment-1.database.windows.net',
+#     'port':'1433',
+#     'user':'ommair-01',
+#     'password':'Omm@ir510219900',
+#     'database':'user_database'
+# }
+
 
 # Establish a connection
 connection = mysql.connector.connect(**config)
