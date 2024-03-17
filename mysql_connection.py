333678e80d6f442c220ab04436ae3711e2032865
Ommair Ahmed <141987574+ommair-01@users.noreply.github.com>
2024-03-17 11:42:56 +0500

    Update mysql_connection.py

--- mysql_connection.py
+++ mysql_connection.py
@@ -1,11 +1,11 @@
 import mysql.connector
 
 config = {
-    'host': 'cloud-assignment-1.database.windows.net',
-    'port': '1433'
-    'user': 'ommair-01',
-    'password': 'Omm@ir510219900',
-    'database': 'cloud-assignment'
+    'host':'cloud-assignment-1.database.windows.net',
+    'port':'1433'
+    'user':'ommair-01',
+    'password':'Omm@ir510219900',
+    'database':'cloud-assignment'
 }
 
 # Establish a connection
