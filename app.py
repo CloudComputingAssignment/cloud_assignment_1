77765389063f0521a41f0a0b461a34d4edbbac0e
Ommair Ahmed <141987574+ommair-01@users.noreply.github.com>
2024-03-17 06:21:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,13 @@
 
-"""
-THIS CODE FILE CONTAINS THE CODE FOR MAIN APP OF THE WEB STORAGE APP.
-
-THIS FILE DEFINES A main() FUNCTION THAT WILL BE EXECUTED WHEN THE APP RUNS. INSIDE THIS FUNTION ALL THE OTHER MENU TABS OF THE APP IS CALLED.
-"""
-
-
 from dependancies import *
 from home import *
 from login import *
+
 from about import *
 
 
-
 def main():
-     #st.set_page_config(
+    # st.set_page_config(
     #     page_title="Cloud Based Web Storage App",
     #     page_icon="home",
     #     layout="wide",
@@ -23,19 +16,19 @@ def main():
     st.title("Cloud Based Web Storage App")
 
 
-     st.sidebar.title("Menu")
-     app_mode = st.sidebar.selectbox('Get Started:', ['User', 'Home',  'About'])
+    st.sidebar.title("Menu")
+    app_mode = st.sidebar.selectbox('Get Started:', ['User', 'Home',  'About'])
 
-     if app_mode == 'User':
-         login()
+    if app_mode == 'User':
+        login()
         
 
-     elif app_mode == 'About':
-         about()
+    elif app_mode == 'About':
+        about()
         
         
-     else:
-         home()
+    else:
+        home()
 
 if __name__ == "__main__":
     try:
